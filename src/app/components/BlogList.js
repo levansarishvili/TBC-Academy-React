93e1a111b26cbf8ed86a88dd6487b05f8c8b79f1
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-20 21:13:54 +0400

    feat: create blog list component for blog page

---
+++ src/app/components/BlogList.js
@@ -0,0 +1,23 @@
+"use client";
+
+import BlogFilter from "../components/BlogFilter";
+import BlogItem from "./BlogItem";
+
+export default function BlogList({ posts }) {
+  return (
+    <div className="blog__page-content">
+      <BlogFilter />
+      <ul className="blog__list">
+        {posts.map((post) => (
+          <BlogItem
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            content={post.body}
+            views={post.views}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
